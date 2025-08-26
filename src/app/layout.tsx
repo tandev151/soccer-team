7d39469a5d3be5c24a2ca9f7e539e023d0763c6a
Le Duc Tan <tanld8@cyberspace.vn>
2025-08-26 17:04:58 +0700

    enhance slide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='theme-dark'>
+    <html lang='en' className='theme-light'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header />
